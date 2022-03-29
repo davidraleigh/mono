8bcc7415339ea044bdfc5c4046f35efee290740c
David Raleigh <davidraleigh@gmail.com>
2022-03-28 21:24:42 -0700

    created geometry

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,5 +6,5 @@ docker run --volume "$(pwd):/workspace" --workdir /workspace/proto bufbuild/buf:
 
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-#docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf:1.3.0 \
-#  breaking --against .git#branch="${branch}"
+docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf:1.3.0 \
+  breaking --against .git#branch="${branch}"
