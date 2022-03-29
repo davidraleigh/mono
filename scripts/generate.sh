cafee97188cef63e470b468ebbef72efdf89ddb8
David Raleigh <davidraleigh@gmail.com>
2022-03-28 21:03:15 -0700

    cleanout old generated code

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,6 +2,13 @@ set -e
 
 ./scripts/lint.sh
 
-rm -rf gen/*
+rm -rf cpp/proto/**/*.pb.cc
+rm -rf cpp/proto/**/*.pb.h
+rm -rf go/proto/**/*.pb.go
+rm -rf java/proto/**/*.java
+rm -rf js/proto/**/*_pb.js
+rm -rf net/proto/**/*.cs
+rm -rf python/proto/**/*_pb2*.py
+rm -rf ruby/proto/**/*_pb*.rb
 
 docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf:1.3.0 generate
