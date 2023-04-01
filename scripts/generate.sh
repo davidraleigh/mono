2f43ff55c431afe622192059c4dc893edd7ec499
David Raleigh <davidraleigh@gmail.com>
2023-04-01 16:04:47 -0700

    grpc and separate gen files

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -6,14 +6,16 @@ set -e
 
 set +e
 
-#rm -rf cpp/proto/**/*.pb.cc
-#rm -rf cpp/proto/**/*.pb.h
-#rm -rf go/proto/**/*.pb.go
-#rm -rf java/proto/**/*.java
-#rm -rf js/proto/**/*_pb.js
-#rm -rf net/proto/**/*.cs
-#rm -rf python/proto/**/*_pb2*.py
-#rm -rf ruby/proto/**/*_pb*.rb
+rm -rf cpp/proto/**/*.pb.cc
+rm -rf cpp/proto/**/*.pb.h
+rm -rf go/proto/**/*.pb.go
+rm -rf java/proto/**/*.java
+rm -rf python/proto/**/*_pb2*.py*
 
 docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf:1.16.0-fix generate --template buf.gen.yaml
+
+rm -rf net/proto/**/*.cs
+rm -rf js/proto/**/*_pb.js
+rm -rf ruby/proto/**/*_pb*.rb
+
 docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf:1.16.0-fix generate --template buf.gen.overflow.yaml
